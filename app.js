af1dbd951ed0f9fbcd34652557c8d7363d35a4df
Alik <A@A.mail>
2025-11-07 05:17:27 +0200

    2.3 udacity

--- app.js
+++ app.js
@@ -19,4 +19,26 @@ nanodegreeCard.textContent = 'The <strong>Greatest</strong> Ice Cream Flavors';
 nanodegreeCard.innerHTML = 'The <strong>Greatest</strong> Ice Cream Flavors';  
 // works as expected
 
+//2.3 udacity
+let cont = document.createElement('span');
+cont.innerText = 'now+';
+let mainH = document.querySelector('h1');
+mainH.appendChild(cont);
 
+const myPara = document.createElement('p');
+const textOfParagraph = document.createTextNode('I am the text for the paragraph!');
+myPara.appendChild(textOfParagraph);
+document.body.appendChild(myPara);
+const myDiv = document.createElement('div');
+myDiv.textContent = 'My DIV';
+document.body.appendChild(myDiv);
+
+let cont2 = document.createElement('span');
+document.body.appendChild(cont2);
+cont2.innerText = 'Hiii';
+document.body.appendChild(cont);
+
+const mainHeading = document.querySelector('#ga-a9c3f5');
+const htmlTextToAdd = '<h2>Skydiving is fun!</h2>';
+
+mainHeading.insertAdjacentHTML('afterbegin', htmlTextToAdd);
